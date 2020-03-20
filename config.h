1529fe4d682e3e3b8c0af960b6da725fcd86afee
Joe Schroedl <joe.schroedl@outlook.com>
2020-03-20 16:08:24 -0400

    Update config.h

--- config.h
+++ config.h
@@ -20,8 +20,8 @@
 //   - Adafruit AirLift Shield -> https://www.adafruit.com/product/4285
 //   - Adafruit AirLift FeatherWing -> https://www.adafruit.com/product/4264
 
-#define WIFI_SSID   "p0cK3T_r0uT3R"
-#define WIFI_PASS   "ncsuengineering"
+#define WIFI_SSID   "ssid"
+#define WIFI_PASS   "password"
 
 // uncomment the following line if you are using airlift
 // #define USE_AIRLIFT
